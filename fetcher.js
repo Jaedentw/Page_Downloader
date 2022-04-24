1b471db67d1f777b2e4d9ef2c8b7ade28fbc632a
Jaeden West <jaeden.t.west@gmail.com>
2022-04-24 17:17:14 -0600

    linted code

--- fetcher.js
+++ fetcher.js
@@ -4,12 +4,12 @@ const myArgs = process.argv.slice(2);
 const url = myArgs[0];
 const path = myArgs[1];
 
-request.get(url, function (err, response, body) {
-  if(err) {
-    console.log(`Error: ${response}`)
+request.get(url, function(err, response, body) {
+  if (err) {
+    console.log(`Error: ${response}`);
   }
-  fs.writeFile(path, body, function () {
-    const bytes = body.length
-    console.log(`Downloaded and saved ${bytes} bytes to ${path}`)
+  fs.writeFile(path, body, function() {
+    const bytes = body.length;
+    console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
   });
-})
+});
